d98a0543d4487aa66d40d68731329ba5aedf335b
PacioTenPros <paciobumbum@gmail.com>
2023-03-13 15:58:07 +0100

    Project cleanup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Kwacker - Modern social platform</title>
+        <meta name="description" content="Created by Pacio with passion" />
+        <link rel="icon" href="/duck.png" />
+      </Head>
+      <main>Welcome in kwacker app 🦆</main>
+    </>
+  );
+};
+
+export default Home;
