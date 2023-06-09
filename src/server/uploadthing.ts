dfec04eafd84b2252920170362c804eb820b0c1f
PacioTenPros <paciobumbum@gmail.com>
2023-06-09 21:09:14 +0200

    I DONT HAVE ANY IDEA

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -7,17 +7,16 @@ const f = createUploadthing()
 export const fileRouter = {
 	postImageUploader: f({ image: { maxFileCount: 1, maxFileSize: '4MB' } })
 		.middleware(async (req, res) => {
-			const data = await getServerSession(req, res, authOptions)
-			console.log(data)
-			if (!data) {
-				throw new Error('Unauthorized')
+			const session = await getServerSession(req, res, authOptions);
+			if (!session?.user) {
+				throw new Error("Unauthorized");
 			}
 
-			return { userId: data.user.id }
+			return { userId: session.user.id }
 		})
 		.onUploadComplete(({ metadata, file }) => {
 			console.log(file.url, metadata.userId)
 		})
 } satisfies FileRouter
 
-export type OurFileRouter = typeof fileRouter
+export type OurFileRouter = typeof fileRouter 
